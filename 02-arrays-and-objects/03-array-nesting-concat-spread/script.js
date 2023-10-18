788d0f78af4d6d25125dfc8226ad17cff1c5b784
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-17 22:22:25 -0500

    populate 03-array-nesting-concat-spread/script.js

--- 02-arrays-and-objects/03-array-nesting-concat-spread/script.js
+++ 02-arrays-and-objects/03-array-nesting-concat-spread/script.js
@@ -1,40 +1,31 @@
 let x;
 
 const fruits = ['apple', 'pear', 'orange'];
-const berries = ['strawberry', 'blueberry', 'rasberry'];
+const berries = ['strawberry', 'blueberry', 'raspberry'];
 
-// Nesting arrays
+// fruits.push(berries);
 
-// Nesting berries inside of fruits
-fruits.push(berries);
+// x = fruits[3][fruits[3].indexOf('blueberry')];
 
-// Now we can access 'blueberry' with the following
-x = fruits[3][1];
+x = allFruits = [fruits, berries];
 
-// Create a new variable and nest both arrays
-const allFruits = [fruits, berries];
+x = allFruits[1][0];
 
-x = allFruits[1][2];
-
-// concat() - Concatenate arrays
 x = fruits.concat(berries);
 
-// spread operator (...) - Concatenate with
+// Spread Operator (...)
 x = [...fruits, ...berries];
 
-// flat() - Flatten an array
-const arr = [1, 2, [3, 4, 5], 6, [7, 8]];
+// Flatten Arrays
+const arr = [1, 2, [3, 4], 5, [6, 7], 8];
 x = arr.flat();
 
-// Static methods of Array object
+// Static Methods on Array Object
 
-// isArray() - Check is is an array
-x = Array.isArray(fruits);
+x = Array.isArray('Hello')
 
-// from() - Create an array from an array like value
 x = Array.from('12345');
 
-// of() - Create an array from a set of values
 const a = 1;
 const b = 2;
 const c = 3;
