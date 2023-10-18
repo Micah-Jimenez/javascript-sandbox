39f57d13e169cdd404ce9804e3635a48c05b0e4c
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-18 18:21:18 -0500

    populate script.js

--- 02-arrays-and-objects/05-object-literals/script.js
+++ 02-arrays-and-objects/05-object-literals/script.js
@@ -1,47 +1,36 @@
 let x;
 
-// Creating an object
 const person = {
   name: 'John Doe',
   age: 30,
   isAdmin: true,
   address: {
-    street: '123 Main st',
-    city: 'Boston',
-    state: 'MA',
+    street: '123 Main St.',
+    city: 'White House',
+    state: 'Tennessee',
   },
   hobbies: ['music', 'sports'],
 };
 
-// Accessing object properties
-x = person.name; // Dot notation
-x = person['age']; // Bracket notation
-x = person.address.state;
+x = person.name;
+x = person['age'];
+x = person.address.city;
 x = person.hobbies[0];
 
-// Updating properties
 person.name = 'Jane Doe';
 person['isAdmin'] = false;
 
-// Deleting properties
 delete person.age;
 
-// Create new properties
-person.hasChildren = true;
+person.hasChildren = false;
 
-// Add functions
 person.greet = function () {
-  console.log(`Hello, my name is ${this.name}`);
-};
+  console.log(`Hello my name is ${this.name}`);
+}
 
 person.greet();
 
-// Keys with multiple words
-const person2 = {
-  'first name': 'Brad',
-  'last name': 'Traversy',
-};
 
-x = person2['first name'];
+x = person;
 
 console.log(x);
