77d64b4ec7bdce3a45c6f0fd8a3aebed1f3a320f
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-19 23:26:38 -0500

    populate script.js

--- 03-functions-scope/10-execution-context-in-action/script.js
+++ 03-functions-scope/10-execution-context-in-action/script.js
@@ -1,9 +1,8 @@
-//  Go to 'sources' tab and add a breakpoint at the first line and step through to see the execution phase in action
 const x = 100;
 const y = 50;
 
 function getSum(n1, n2) {
-  const sum = n1 + n2;
+  sum = n1 + n2;
   return sum;
 }
 
