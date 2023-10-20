d9465cffbff472815b597797bd06d7c30a2bb2a4
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-19 20:12:12 -0500

    populate script.js

--- 03-functions-scope/03-global-function-scope/script.js
+++ 03-functions-scope/03-global-function-scope/script.js
@@ -1,33 +1,34 @@
-// 'window' is globally scoped
-// alert('Hello');
-// console.log(innerWidth);
+// window.alert("Hello");
+// console.log(window.innerWidth);
 
-// Global scope variable
 const x = 100;
-
 console.log(x, 'in global');
 
-function run() {
-  // Access global vars in functions
-  console.log(window.innerHeight);
-  console.log(x, 'in function');
+function main() {
+
+  run();
+
+  console.log(x, 'in main function');
+
+  if (true) {
+    console.log(x, 'in block');
+  }
+
+  // console.log(y); ERROR
+  add();
+
 }
 
-run();
 
-// Access global vars in blocks
-if (true) {
-  console.log(x, 'in block');
+function run() {
+  console.log(window.innerHeight);
 }
 
 function add() {
-  // Overwriting global x (variable shadowing)
-  const x = 1;
+  const x = 1 // this overwrites the global x variable this is called 'variable shadowing'
   const y = 50;
   console.log(x + y);
 }
 
-// Can not access a function scoped variable in global scope
-console.log(y);
 
-add();
+main();
