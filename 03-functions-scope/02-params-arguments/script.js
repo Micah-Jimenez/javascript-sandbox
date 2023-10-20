6e5b733a83ec0bd77508b8f6502b77fa0874e82c
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-19 19:54:43 -0500

    populate script.js

--- 03-functions-scope/02-params-arguments/script.js
+++ 03-functions-scope/02-params-arguments/script.js
@@ -1,53 +1,45 @@
-// Default Params
-function registerUser(user = 'Bot') {
-  // Old way - before using `=`
-  // if (!user) {
-  //   user = 'Bot';
-  // }
-
-  return user + ' is registered';
-}
-
-console.log(registerUser());
-
-// Rest Params
-function sum(...numbers) {
-  let total = 0;
-
-  for (const num of numbers) {
-    total += num;
+function main() {
+  // Default Params
+  function registerUser(user = "Bot") {
+    // if (!user) {
+    //   user = 'Bot'
+    // }
+    return user + " is registered";
   }
 
-  return total;
+  console.log(registerUser());
+
+  // Rest Params
+  function sum(...numbers) {
+    // The rest operator allows you to input an unlimited amount of arguments to the function by putting all the arguments into an array
+    const n = numbers.length;
+    let total = 0;
+    for (const num of numbers) {
+      total += num;
+    }
+    return total;
+  }
+
+  console.log(sum(1, 2, 3));
+
+  // Objects as Params
+  function loginUser(user = { id: 0, name: "Mr. Doe" }) {
+    return `The user ${user.name} with the id of ${user.id} is logged in`;
+  }
+
+  const user = {
+    id: 1,
+    name: "Micah",
+  };
+
+  console.log(loginUser(user));
+
+  // Arrays as Params
+  function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  console.log(getRandom([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 }
 
-console.log(sum(1, 2, 3, 4, 5, 6, 100));
-
-// Objects as params
-function loginUser(user) {
-  return `The user ${user.name} with the id of ${user.id} is logged in`;
-}
-
-const user = {
-  id: 1,
-  name: 'John',
-};
-
-console.log(loginUser(user));
-console.log(
-  loginUser({
-    id: 2,
-    name: 'Sara',
-  })
-);
-
-// Arrays as params
-function getRandom(arr) {
-  const randomIndex = Math.floor(Math.random() * arr.length);
-
-  const item = arr[randomIndex];
-
-  console.log(item);
-}
-
-getRandom([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+main();
