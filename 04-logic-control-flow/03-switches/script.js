6ae76ce4b87bdf97986701e4a1f5615ecab228c3
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-22 17:03:05 -0500

    populate script.js

--- 04-logic-control-flow/03-switches/script.js
+++ 04-logic-control-flow/03-switches/script.js
@@ -1,8 +1,7 @@
-const d = new Date(2022, 1, 10, 19, 0, 0);
+const d = new Date(2023, 1, 10, 8, 0, 0);
 const month = d.getMonth();
 const hour = d.getHours();
 
-// Immediate value evaluation
 switch (month) {
   case 1:
     console.log('It is January');
@@ -14,10 +13,9 @@ switch (month) {
     console.log('It is March');
     break;
   default:
-    console.log('It is not Jan, Feb or March');
+    console.log('It is not Jan, Feb, or Mar');
 }
 
-// Range evaluation
 switch (true) {
   case hour < 12:
     console.log('Good Morning');
@@ -27,4 +25,5 @@ switch (true) {
     break;
   default:
     console.log('Good Night');
+  
 }
