17463a6624152296f204300d02fb4d965a5e1abb
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-22 15:21:36 -0500

    populate script.js

--- 04-logic-control-flow/01-if-statements/script.js
+++ 04-logic-control-flow/01-if-statements/script.js
@@ -1,36 +1,33 @@
-// If Statement Syntax
 if (true) {
-  console.log('This is true');
+  console.log("This is true");
 }
 
 if (false) {
-  console.log('This is NOT true');
+  console.log("This is false");
 }
 
-// Evaluation expressions
 const x = 10;
 const y = 5;
 
 if (x >= y) {
-  console.log(`${x} is greater than or equal to ${y}`);
+    console.log(`${x} is greater than or equal to ${y}`);
 }
 
 if (x === y) {
-  console.log(`${x} is equal to ${y}`);
+    console.log(`${x} is equal to ${y}`);
 } else {
-  console.log(`${x} is NOT equal to ${y}`);
+    console.log(`${x} is not equal to ${y}`);
 }
 
-// Block scope
-if (x !== y) {
-  const z = 20;
-  console.log(`${z} is 20`);
+if (x != y) {
+    let z = 20;
+    console.log(`z is ${z}`);
 }
 
-console.log(z); // Throw error
+// console.log(z);
 
-// Shorthand If/Else
-if (x >= y)
-  console.log(`${x} is greater than or equal to ${y}`),
+// Shorthand If
+if (x >= y) 
+    console.log(`${x} is greater than or equal to ${y}`),
     console.log('This is true');
 else console.log('This is false');
