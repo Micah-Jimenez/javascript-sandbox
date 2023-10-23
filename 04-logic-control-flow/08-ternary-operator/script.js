1d28e387b03ab7e267ae5b86c1790b53dfa52d05
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-22 23:06:59 -0500

    populate script.js

--- 04-logic-control-flow/08-ternary-operator/script.js
+++ 04-logic-control-flow/08-ternary-operator/script.js
@@ -1,18 +1,18 @@
-const age = 17;
+const age = 19;
 
 // Using an if statement
 if (age >= 18) {
   console.log('You can vote!');
 } else {
-  console.log('You can not vote');
+  console.log('You can not vote.');
 }
 
 // Using a ternary operator
-age >= 18 ? console.log('You can vote!') : console.log('You can not vote');
+age >= 18 ? console.log('You can vote!') : console.log('You can not vote.');
 
 // Assigning a conditional value to a variable
 const canVote = age >= 18 ? true : false;
-const canVote2 = age >= 18 ? 'You can vote!' : 'You can not vote';
+const canVote2 = age >= 18 ? 'Yes' : 'No';
 
 console.log(canVote);
 console.log(canVote2);
@@ -20,8 +20,6 @@ console.log(canVote2);
 // Multiple statements
 
 const auth = true;
-
-// Long version
 // let redirect;
 
 // if (auth) {
@@ -32,14 +30,12 @@ const auth = true;
 //   redirect = '/login';
 // }
 
-// Shorter ternary version
-const redirect = auth
-  ? (alert('Welcome to the dashbaord'), '/dashboard')
-  : (alert('Access Denied'), '/login');
+// const redirect = auth 
+// ? (alert('Welcome to the dashboard'), '/dashboard') 
+// : (alert('Access Denied'), '/login');
 
-console.log(redirect);
+// console.log(redirect);
 
-// Ternary with no else
 auth ? console.log('Welcome to the dashboard') : null;
-// Shorthand for ternary with no else
-auth && console.log('Welcome to the dashoard');
+
+auth && console.log('Welcome to the dashboard');
