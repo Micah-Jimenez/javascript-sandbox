6196a7bfe6fd551c2c6e62eb516f66b85550b27f
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-24 23:15:43 -0500

    populate script.js

--- 05-iteration-array-methods/01-for-loop/script.js
+++ 05-iteration-array-methods/01-for-loop/script.js
@@ -6,30 +6,24 @@
 // INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
 // STATEMENT - Code that will be executed each time the loop is run. To execute a `block` of code, use the `{}` syntax
 
-// for (let i = 0; i <= 10; i++) {
-//   if (i === 7) {
-//     console.log('7 is my lucky number');
-//   } else {
-//     console.log('Number ' + i);
-//   }
-// }
+for (let i = 0; i < 10; i++) {
+    console.log('number', i);
+}
 
 // Nested loops
-// for (let i = 1; i <= 10; i++) {
-//   console.log('Number ' + i);
-
-//   for (let j = 1; j <= 5; j++) {
-//     console.log(`${i} * ${j} = ${i * j}`);
-//   }
-// }
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
+}
 
 // Loop through an array
-const names = ['Brad', 'Sam', 'Sara', 'John', 'Tim'];
-
-for (let i = 0; i < names.length; i++) {
-  if (names[i] === 'John') {
-    console.log(names[i] + ' is the best');
-  } else {
-    console.log(names[i]);
-  }
+const names = ['Brad', 'Micah', 'John', 'Tim'];
+const n = names.length;
+for (let i = 0; i < n; i++) {
+    if (names[i] === 'Micah'){
+        console.log('Micah is the best!');
+    } else {
+        console.log(names[i]);
+    }  
 }
