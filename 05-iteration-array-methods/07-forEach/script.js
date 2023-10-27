2639a9637c85b8ece81cc818f8cefa1d07c3f67d
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-26 19:53:09 -0500

    populate script.js

--- 05-iteration-array-methods/07-forEach/script.js
+++ 05-iteration-array-methods/07-forEach/script.js
@@ -1,32 +1,20 @@
 const socials = ['Twitter', 'LinkedIn', 'Facebook', 'Instagram'];
 
-// View prototype chain
-console.log(socials.__proto__);
+// console.log(socials.__proto__);
 
-// Long form
-socials.forEach(function (item) {
-  console.log(item);
-});
+// socials.forEach((n, index, arr) => console.log(`${index} - ${n}`, arr));
 
-// Short form
-socials.forEach((item) => console.log(item));
-
-// We can also pass in the index and original array
-socials.forEach((item, index, arr) => console.log(`${index} - ${item}`, arr));
-
-// Using a named function
 function logSocials(social) {
   console.log(social);
 }
 
-socials.forEach(logSocials);
+socials.forEach(logSocials)
 
-// Array of objects
 const socialObjs = [
-  { name: 'Twitter', url: 'https://twitter.com' },
-  { name: 'Facebook', url: 'https://facebook.com' },
-  { name: 'Linkedin', url: 'https://linkedin.com' },
-  { name: 'Instagram', url: 'https://instagram.com' },
-];
+  {name: 'Twitter', url: 'https://twitter.com'},
+  {name: 'Facebook', url: 'https://facebook.com'},
+  {name: 'Linkedin', url: 'https://linkedin.com'},
+  {name: 'Instagram', url: 'https://instagram.com'},
+]
 
-socialObjs.forEach((item) => console.log(item.url));
+socialObjs.forEach(n => console.log(n.url))
