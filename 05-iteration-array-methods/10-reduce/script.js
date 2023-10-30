37fb8cd823af801ade4e781187c13a1aafcc8531
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-10-30 00:00:03 -0500

    populate script.js

--- 05-iteration-array-methods/10-reduce/script.js
+++ 05-iteration-array-methods/10-reduce/script.js
@@ -1,30 +1,28 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Add all numbers together
-const sum = numbers.reduce(function (accumulator, currentValue) {
-  return accumulator + currentValue;
-}, 0);
+// const sum = numbers.reduce(function (accumulator, currentValue) {
+//     return accumulator + currentValue;
+// }, 0)
 
-const sum2 = numbers.reduce((acc, cur) => acc + cur, 0);
+const sum = numbers.reduce((acc, curr) => acc + curr, 10);
 
 // Using a for loop
 const sum3 = () => {
-  let acc = 0;
-  for (const cur of numbers) {
-    acc += cur;
-  }
-  return acc;
-};
+    let sum = 0;
+    for (const i of numbers) {
+        sum += i;
+    }
+    return sum;
+}
 
-// Shopping cart example (objects)
+console.log(sum3());
+
 const cart = [
-  { id: 1, name: 'Product 1', price: 130 },
-  { id: 2, name: 'Product 2', price: 150 },
-  { id: 3, name: 'Product 3', price: 200 },
-];
+    {id: 1, name: 'Product 1', price: 130},
+    {id: 2, name: 'Product 2', price: 150},
+    {id: 3, name: 'Product 3', price: 175}
+]
 
-const total = cart.reduce(function (acc, product) {
-  return acc + product.price;
-}, 0);
+const total = cart.reduce((acc, curr) => acc + curr.price, 0);
 
 console.log(total);
