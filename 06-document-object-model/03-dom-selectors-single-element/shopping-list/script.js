98feaca4293622af3dc10b89b8436c4a03e4f718
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-11-02 21:10:34 -0500

    complete section

--- 06-document-object-model/03-dom-selectors-single-element/shopping-list/script.js
+++ 06-document-object-model/03-dom-selectors-single-element/shopping-list/script.js
@@ -1,8 +1,5 @@
 // document.getElementById()
-
 console.log(document.getElementById('app-title'));
-
-// Get attributes
 console.log(document.getElementById('app-title').id);
 console.log(document.getElementById('app-title').className);
 console.log(document.getElementById('app-title').getAttribute('id'));
@@ -12,34 +9,39 @@ document.getElementById('app-title').title = 'Shopping List';
 document.getElementById('app-title').setAttribute('class', 'title');
 
 const title = document.getElementById('app-title');
+console.log(title);
 
-// Get/change content
-console.log(title.textContent);
+// Get/Change content
+
+// if (prompt('password: ') === '1234') {
+//     title.textContent = 'Welcome back!'
+// } else {
+//     title.innerText = 'Hi, new user!'
+// }
+
 title.textContent = 'Hello World';
 title.innerText = 'Hello Again';
 title.innerHTML = '<strong>Shopping List</strong>';
 
 // Change styles
 title.style.color = 'red';
-title.style.backgroundColor = 'black';
+title.style.backgroundColor = 'pink';
 title.style.padding = '10px';
 title.style.borderRadius = '10px';
 
 // document.querySelector()
-
-// Use any CSS selector
 console.log(document.querySelector('h1'));
 console.log(document.querySelector('#app-title'));
 console.log(document.querySelector('.container'));
 console.log(document.querySelector('input[type="text"]'));
 console.log(document.querySelector('li:nth-child(2)').innerText);
 
-const secondItem = document.querySelector('li:nth-child(2)');
-secondItem.innerText = 'Apple Juice';
-secondItem.style.color = 'red';
+let secondItem = document.querySelector('li:nth-child(2)');
+secondItem.innerText = 'Water';
+secondItem.style.color = 'blue'
 
-// Use these methods on other elements
+// Use the methods on other elements
 const list = document.querySelector('ul');
 console.log(list);
-const firstItem = list.querySelector('li');
-firstItem.style.color = 'blue';
+firstItem = list.querySelector('li');
+firstItem.style.color = 'red';
