4a3851e7339ab2ace9f0e80f2230333f25991464
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-11-05 21:33:22 -0600

    populate script.js

--- 06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
+++ 06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
@@ -1,29 +1,28 @@
-function createNewItem(item) {
+function createNewItem(item){
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(item));
 
   const button = createButton('remove-item btn-link text-red');
 
+  const icon = createIcon('fa-solid fa-xmark');
+
+  button.appendChild(icon);
   li.appendChild(button);
 
   document.querySelector('.items').appendChild(li);
-}
+};
 
 function createButton(classes) {
   const button = document.createElement('button');
   button.className = classes;
-
-  const icon = createIcon('fa-solid fa-xmark');
-  button.appendChild(icon);
-
   return button;
-}
+};
 
 function createIcon(classes) {
   const icon = document.createElement('i');
   icon.className = classes;
   return icon;
-}
+};
 
 createNewItem('Cheese');
-createNewItem('Sauce');
+createNewItem('Sauce')
