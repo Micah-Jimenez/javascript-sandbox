c1b83dbd2a481fbe52f1bde6c850b8c0a3f56acf
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-11-05 23:15:32 -0600

    populate script.js

--- 06-document-object-model/13-remove-elements/shopping-list/script.js
+++ 06-document-object-model/13-remove-elements/shopping-list/script.js
@@ -1,42 +1,30 @@
-// remove() Method
-function removeClearButton() {
-  const clearBtn = document.querySelector('#clear');
-  clearBtn.remove();
-}
+function removeClearBtn() {
+  document.querySelector('#clear').remove();
+};
 
-// removeChild() Method
+removeClearBtn();
+
+// 
 function removeFirstItem() {
   const ul = document.querySelector('ul');
-  const li = document.querySelector('li:first-child');
+  const li = document.querySelector('li');
 
   ul.removeChild(li);
 }
 
-// Other examples
+// removeFirstItem();
 
+// 
 function removeItem(itemNumber) {
   const ul = document.querySelector('ul');
   const li = document.querySelector(`li:nth-child(${itemNumber})`);
 
   ul.removeChild(li);
 }
-
 function removeItem2(itemNumber) {
-  const ul = document.querySelector('ul');
-  const li = document.querySelectorAll('li')[itemNumber - 1];
-
-  ul.removeChild(li);
+  const arr = document.querySelectorAll('li');
+  arr[itemNumber - 1].remove();
 }
 
-function removeItem3(itemNumber) {
-  const li = document.querySelectorAll('li');
-  li[itemNumber - 1].remove();
-}
-
-const removeItem4 = (itemNumber) =>
-  document.querySelectorAll('li')[itemNumber - 1].remove();
-
-removeClearButton();
-// removeFirstItem();
-// removeItem(2);
-removeItem4(2);
+// removeItem(3);
+removeItem2(4);
