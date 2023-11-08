e498b6d4c99e12a5d7a58f8aed7fd2df69c38c05
Micah-Jimenez <131635287+Micah-Jimenez@users.noreply.github.com>
2023-11-07 21:45:36 -0600

    populate script.js

--- 07-events/06-input-events/shopping-list/script.js
+++ 07-events/06-input-events/shopping-list/script.js
@@ -4,27 +4,26 @@ const checkbox = document.getElementById('checkbox');
 const heading = document.querySelector('h1');
 
 function onInput(e) {
-  heading.textContent = e.target.value;
+  console.log(e.target.value);
 }
 
 function onChecked(e) {
-  const isChecked = e.target.checked;
-  heading.textContent = isChecked ? 'Checked' : 'Not Checked';
+  console.log(e.target.checked);
+  heading.textContent = e.target.checked ? 'Checked!' : 'Shopping List';
 }
 
-function onFocus() {
-  console.log('Input is focused');
+function onFocus(e) {
+  console.log('input is focused');
   itemInput.style.outlineStyle = 'solid';
   itemInput.style.outlineWidth = '1px';
-  itemInput.style.outlineColor = 'red';
+  itemInput.style.outlineColor = 'green';
 }
 
-function onBlur() {
-  console.log('Input is not focused');
+function onBlur(e) {
+  console.log('input is not focused');
   itemInput.style.outlineStyle = 'none';
 }
 
-// input, change, focus and blur events
 itemInput.addEventListener('input', onInput);
 priorityInput.addEventListener('change', onInput);
 checkbox.addEventListener('input', onChecked);
